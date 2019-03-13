de6c1a25449828caf7f2d3076ba53bded768020b
Lorna Powell <powell.lap@gmail.com>
2019-03-13 16:19:01 +0000

    Timeout ()

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ xhr.send();
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        data = this.responseText;
+        data = JSON.parse(this.responseText);
     };
 }
 
-console.log(data);
+setTimeout(function() {
+    console.log(data);
+}, 500);
