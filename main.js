2a0fdb336e5705a401d6900e1ea506e6d7d7874f
Lorna Powell <powell.lap@gmail.com>
2019-03-13 16:30:36 +0000

    Data and the DOM more tabular data

--- main.js
+++ main.js
@@ -28,11 +28,17 @@ function writeToDocument(type) {
     el.innerHTML = "";
 
     getData(type, function(data) {
+        var tableRows = [];
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
-            // el.innerHTML += "<p>" + item.name + "</p>";
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow)
         });
 
         el.innerHTML = `<table>${tableHeaders}</table>`;
